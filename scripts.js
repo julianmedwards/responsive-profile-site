ef01f4dfcfe1e9d25e1202da11a45043dce0069b
julianmedwards <julian.michael.edwards@gmail.com>
2022-01-25 12:00:46 -0600

    Func moves bio under name/title on views > mobile.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 const profileImg = new Image()
-// Finds dimensions of selected profile image and inserts values into
+// *Mobile - Finds dimensions of selected profile image and inserts values into
 // CSS calculation of div height.
 profileImg.onload = function () {
     let root = document.querySelector(':root')
@@ -14,7 +14,25 @@ profileImg.onload = function () {
 }
 profileImg.src = '/img/julianprague.jpg'
 
-// Toggle off-canvas nav menu on phone.
+// Move bio elements into correct flex container on devices larger than
+// mobile.
+const mainTitle = document.getElementById('main-title')
+const bio = document.getElementById('bio')
+const content = document.getElementById('content')
+
+function moveBio() {
+    let viewportWidth =
+        window.innerWidth || document.documentElement.clientWidth
+    if (viewportWidth > 480 && bio.parentNode === content) {
+        mainTitle.appendChild(bio)
+    } else if (viewportWidth <= 480 && bio.parentNode === mainTitle) {
+        content.appendChild(bio)
+    }
+}
+window.onload = moveBio
+window.addEventListener('resize', moveBio)
+
+// Off-canvas toggle nav menu on phone.
 function toggleNav() {
     let navmenu = document.getElementById('navmenu')
     if (navmenu.style.width != '100%') {
